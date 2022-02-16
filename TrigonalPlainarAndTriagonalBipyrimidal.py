02921bcd89cd49686bdde326e3c6ca19abeee343
MickieNewman1 <99620636+MickieNewman1@users.noreply.github.com>
2022-02-16 17:21:16 +0000

    Add files via upload

---
+++ TrigonalPlainarAndTriagonalBipyrimidal.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 13th 03:57:04 2021
+
+@author: mickie newman
+"""
+from turtle import * #fetches turtle commands
+from random import * #fetches random number commands
+import numpy as np
+
+win = Screen()
+win.bgcolor("#003249") #dark blue background
+
+#hex colors
+lightGray ='#CCDBDC'
+purpleBlue = '#7692FF'
+seaBlue = '#9AD1D4'
+blueSky = '#7ED4E6'
+bluerBlue = '#3BA99C' 
+
+crystal = Turtle(visible=False)# so we dont see the turtle walking around ruens the imersion
+
+crystal.speed("fastest")
+
+crystal.width(3)
+
+crystal.up()
+
+colors = [lightGray , purpleBlue, seaBlue, blueSky, bluerBlue]#list of colors for the program to choose from
+
+for i in range(randint(10, 50)): #randomizes how many times the loop is run
+    crystal.color(choice(colors)) #choosing a random color
+    crystal.goto(randint(-100,100), randint(-100,100)) #choosing a random starting point
+    crystal.down()
+   
+    for i in range(randint(1, 50) ): #randomizes how long a single line goes for
+        crystal.forward(50) #all segments are 50 long
+        crystal.right(randint(-10, 10)*120) #randomizes which angle the turtle turns to, but in multiples of 120 degrees the bond angles for trigonal plainar triagonal bipyrimidal.
+        
+done()
---
+++ octahedral.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 13th 03:57:04 2021
+
+@author: mickie newman
+"""
+from turtle import * #fetches turtle commands
+from random import * #fetches random number commands
+import numpy as np
+
+win = Screen()
+win.bgcolor("#003249") #dark blue background
+
+#hex colors
+lightGray ='#CCDBDC'
+purpleBlue = '#7692FF'
+seaBlue = '#9AD1D4'
+blueSky = '#7ED4E6'
+bluerBlue = '#3BA99C' 
+
+crystal = Turtle(visible=False)# so we dont see the turtle walking around ruens the imersion
+
+crystal.speed("fastest")
+
+crystal.width(3)
+
+crystal.up()
+
+colors = [lightGray , purpleBlue, seaBlue, blueSky, bluerBlue]#list of colors for the program to choose from
+
+for i in range(randint(10, 50)): #randomizes how many times the loop is run
+    crystal.color(choice(colors)) #choosing a random color
+    crystal.goto(randint(-100,100), randint(-100,100)) #choosing a random starting point
+    crystal.down()
+    
+    for i in range(randint(1, 50) ): #randomizes how long a single line goes for
+        crystal.forward(50) #all segments are 50 long
+        crystal.right(randint(-10, 10)*90) #randomizes which angle the turtle turns to, but in multiples of 90 degrees.
+        #This is because due to the types of bonds in octahedral structures tend to have angles that are multiples of 90 degrees
+done()
